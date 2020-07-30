85007e7c0363a2353cef52d8cbd54c50e3c6cbb6
Marvin Countryman <me@maar.vin>
2020-07-29 21:05:51 -0400

    Add working corehost_sys build configuration for macos

--- corehost_sys/build.rs
+++ corehost_sys/build.rs
@@ -1,11 +1,10 @@
-use cmake::Config;
-use std::process::{Command, Stdio};
-use std::fs;
-use std::io::Read;
 use std::error::Error;
 use std::path::PathBuf;
+use std::process::{Command, Stdio};
+use std::{env, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
+  // Get commit hash from git
   let hash = String::from_utf8(
     Command::new("git")
       .arg("rev-parse")
@@ -14,15 +13,19 @@ fn main() -> Result<(), Box<dyn Error>> {
       .stdout(Stdio::piped())
       .spawn()?
       .wait_with_output()?
-      .stdout
+      .stdout,
   )?;
 
+  let out = env::var("OUT_DIR")?;
+  let out = PathBuf::from(out);
   let hash = hash.trim();
-  let corhost = get_absolute("../vendor/dotnet/runtime/src/installer/corehost")?;
+  let arch = get_arch()?;
   let native = get_absolute("../vendor/dotnet/runtime/eng/native")?;
+  let corhost = get_absolute("../vendor/dotnet/runtime/src/installer/corehost")?;
 
-  let dest = Config::new(corhost)
-    .define("CLR_CMAKE_HOST_ARCH", "x64")
+  // Build dotnet corehost CMakeList.txt
+  let target = cmake::Config::new(corhost)
+    .define("CLR_CMAKE_HOST_ARCH", arch)
     .define("CLR_ENG_NATIVE_DIR", native)
     .define("CLI_CMAKE_HOST_POLICY_VER", "3.0.0")
     .define("CLI_CMAKE_HOST_FXR_VER", "3.0.0")
@@ -33,13 +36,51 @@ fn main() -> Result<(), Box<dyn Error>> {
     .define("CLI_CMAKE_PORTABLE_BUILD", "1")
     .build();
 
+  // Generate bindings
+  let bindings = bindgen::builder()
+    .header(target.join("corehost/nethost.h").to_str().unwrap())
+    .whitelist_type("get_hostfxr_parameters")
+    .whitelist_type("get_hostfxr_path")
+    .generate()
+    .unwrap();
+
+  bindings.write_to_file(out.join("bindings.rs"))?;
+
+  // Link nethost static library
+  println!(
+    "cargo:rustc-link-search=native={}",
+    target.join("corehost").display()
+  );
+  println!("cargo:rustc-link-lib=static=nethost");
+
+  // Link c++
+  match env::var("CARGO_CFG_TARGET_OS")?.as_str() {
+    "macos" | "ios" => println!("cargo:rustc-link-lib=dylib=c++"),
+    "linux" => println!("cargo:rustc-link-lib=dylib=stdc++"),
+    _ => unimplemented!(),
+  };
+
   Ok(())
+}
+
+fn get_arch() -> Result<String, Box<dyn Error>> {
+  let arch = env::var("CARGO_CFG_TARGET_ARCH")?;
+  match arch.as_str() {
+    "x86_64" => Ok("AMD64".to_string()),
+    arch => Ok(arch.to_string()),
+  }
 }
 
 fn get_absolute<S: Into<String>>(path: S) -> Result<String, Box<dyn Error>> {
   let path = PathBuf::from(path.into());
-  let path = fs::canonicalize(&path).expect("the path couldn't be canonicalized");
-  let path = path.to_str().expect("the path couldn't be converted to &str");
+  let path = fs::canonicalize(&path).expect(&*format!(
+    "the path '{}' couldn't be canonicalized",
+    path.display()
+  ));
+  let path = path.to_str().expect(&*format!(
+    "the path '{}' couldn't be converted to &str",
+    path.display()
+  ));
 
   Ok(path.replace("\\\\?\\", ""))
 }
