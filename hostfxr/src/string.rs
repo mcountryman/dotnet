3cba36491fd00bdaacbbb51ffa9eb0cb3ae96fa1
Marvin Countryman <me@maar.vin>
2021-01-13 11:03:12 -0500

    Document and test hostfxr::string

--- hostfxr/src/string.rs
+++ hostfxr/src/string.rs
@@ -1,32 +1,50 @@
-use crate::parameters::HostFxrParameters;
-use hostfxr_sys::hostfxr_initialize_parameters;
-/// Lightweight wide/short null suffix string conversion.
-use std::borrow::Cow;
+//! Compile time `char_t` conversion to and from std types.
+//!
+//! Due to `hostfxr` APIs using `c_wchar` and `c_char` in different builds an abstraction
+//! for both are needed.  
 use std::mem::size_of;
 use std::mem::MaybeUninit;
 use std::os::raw::c_char;
 use std::slice::from_raw_parts;
 
-#[derive(Debug, Clone)]
-pub struct WideString<'a>(Cow<'a, str>);
+use hostfxr_sys::hostfxr_initialize_parameters;
 
+use crate::parameters::HostFxrParameters;
+
+/// A value-to-vec conversion with items of variable size.
 pub trait IntoBytes<T> {
+  /// Performs the conversion.
   fn into_bytes(self) -> Vec<T>;
 }
 
+/// A value-to-pointer conversion with items of variable size.
 pub trait IntoPtr<T> {
+  /// Performs the conversion.
   unsafe fn into_ptr(self) -> *const T;
 }
 
-pub trait IntoMutPtr<T> {
-  unsafe fn into_mut_ptr(self) -> *mut T;
-}
-
+/// Convert to string, consuming value.
 pub trait IntoString<'a> {
+  /// Performs the conversion.
   fn into_string(self) -> String;
 }
 
+impl<'a> IntoString<'a> for &'a [c_char] {
+  /// Copies nul terminated `c_char` ptr into utf8 decoded string.
+  fn into_string(self) -> String {
+    let len = self.len();
+    let buf = unsafe { from_raw_parts(self.as_ptr() as *const _, len) };
+
+    String::from_utf8_lossy(buf).to_string()
+  }
+}
+
 impl<'a> IntoString<'a> for *const c_char {
+  /// Copies nul terminated `c_char` ptr into utf8 decoded string.
+  ///
+  /// # Safety
+  /// If supplied ptr is not nul terminated, nul terminator loop will continue forever or
+  /// until segfault.
   fn into_string(self) -> String {
     let len = (0..).position(|i| unsafe { *self.offset(i) == 0 }).unwrap();
     let buf = unsafe {
@@ -41,18 +59,13 @@ impl<'a> IntoString<'a> for *const c_char {
   }
 }
 
-impl<'a> IntoString<'a> for &'a [c_char] {
-  fn into_string(self) -> String {
-    let len = self.len();
-    let buf = self.as_ptr();
-    let buf = unsafe { from_raw_parts(buf as *const _, len) };
-
-    String::from_utf8_lossy(buf).to_string()
-  }
-}
-
 #[cfg(windows)]
 impl<'a> IntoString<'a> for *const u16 {
+  /// Copies nul terminated `c_wchar` ptr into wtf8 decoded string.
+  ///
+  /// # Safety
+  /// If supplied ptr is not nul terminated, nul terminator loop will continue forever or
+  /// until segfault.
   fn into_string(self) -> String {
     let len = (0..).position(|i| unsafe { *self.offset(i) == 0 }).unwrap();
     let buf = unsafe {
@@ -131,20 +144,6 @@ impl<R: IntoBytes<u16>> IntoPtr<u16> for R {
     let ptr = buf.as_ptr();
 
     std::mem::forget(buf);
-
-    ptr
-  }
-}
-
-impl<V, I: IntoIterator<Item = R>, R: IntoPtr<V>> IntoMutPtr<*const V> for I {
-  unsafe fn into_mut_ptr(self) -> *mut *const V {
-    let mut vec: Vec<*const V> = self //
-      .into_iter()
-      .map(|item| item.into_ptr())
-      .collect();
-    let ptr = vec.as_mut_ptr();
-
-    std::mem::forget(vec);
 
     ptr
   }
@@ -251,6 +250,39 @@ mod tests {
       );
 
       assert_eq!(&buf[buf.len() - 1], &0, "Bytes are not null terminated");
+    }
+  }
+
+  #[test]
+  fn test_into_str_from_c_char_ptr() {
+    let cases = vec![
+      (&b"Hello World\0"[..], "Hello World"),
+      (&b"Hello \0World"[..], "Hello "),
+      (&b"\0"[..], ""),
+    ];
+
+    for (buf, expected) in cases {
+      let buf = buf.as_ptr() as *const c_char;
+      let actual = buf.into_string();
+
+      assert_eq!(actual, expected);
+    }
+  }
+
+  #[test]
+  #[cfg(windows)]
+  fn test_into_str_from_wide_ptr() {
+    let cases: Vec<(&[u16], &str)> = vec![
+      (wchar::wch!("Hello World\0"), "Hello World"),
+      (wchar::wch!("Hello \0World"), "Hello "),
+      (wchar::wch!("\0"), ""),
+    ];
+
+    for (buf, expected) in cases {
+      let buf = buf.as_ptr();
+      let actual = buf.into_string();
+
+      assert_eq!(actual, expected);
     }
   }
 }
